46f7e69da0b354e7e9f90dc789214e601993c542
johan97lund <johan.lund97@gmail.com>
2023-05-04 17:32:33 +0200

    Initial commit

---
+++ src/main/java/FacebookLoginTest.java
@@ -0,0 +1,92 @@
+/**
+ * @author Johan Lund
+ * @project FacebookTests
+ * @date 2023-04-26
+ */
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FacebookLoginTest {
+
+    static Logger logger = LoggerFactory.getLogger(FacebookLoginTest.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        logger.info("Starting the test");
+
+        String email = null;
+        String password = null;
+        try {
+            // Read the email and password from config.json
+            Gson gson = new Gson();
+            JsonElement config = gson.fromJson(new FileReader("config.json"), JsonElement.class);
+            email = config.getAsJsonObject().get("email").getAsString();
+            password = config.getAsJsonObject().get("password").getAsString();
+        } catch (IOException e) {
+            // Handle any exceptions that might occur while reading the file
+            e.printStackTrace();
+        }
+
+        WebDriver driver = null;
+        try {
+            // Set the path to the ChromeDriver executable
+            System.setProperty("/users/johanlund/Downloads/Chromedriver_mac64\\chromedriver", "path/to/chromedriver");
+
+            // Creates an instance of ChromeOptions and add the desired option
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            options.addArguments("--remote-debugging-port=9222");
+
+            // Launch ChromeDriver
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+        } catch (WebDriverException e) {
+            // Handle any exceptions that might occur while launching the ChromeDriver
+            e.printStackTrace();
+        }
+
+
+        // Go to the Facebook login page
+        assert driver != null;
+        driver.get("https://www.facebook.com/login.php");
+
+        WebElement button = driver.findElement(By.xpath("//button[@data-testid='cookie-policy-manage-dialog-accept-button']"));
+        button.click();
+
+        // Enter email address
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys(email);
+
+        // Enter password
+        WebElement passwordField = driver.findElement(By.id("pass"));
+        passwordField.sendKeys(password);
+
+        // Click the "Log In" button
+        WebElement loginButton = driver.findElement(By.name("login"));
+        loginButton.click();
+
+        // Wait for the login process to complete
+        Thread.sleep(5000);
+
+        // Profile menu click
+        WebElement profilePic = driver.findElement(By.xpath("//*[@aria-label='Your profile']"));
+        profilePic.click();
+
+        // Wait for the logout process to complete
+        Thread.sleep(5000);
+
+        // Close the browser
+        driver.quit();
+    }
+    }
+
